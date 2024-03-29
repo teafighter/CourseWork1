5695ebb6367cdad5adfac41e34fb9d8228275ab5
Kirill Tyurikov <teafighter@gmail.com>
2024-03-29 20:32:17 +0300

    сдача на проверку самой высокой сложности

---
+++ src/EmployeeBook.java
@@ -0,0 +1,228 @@
+public class EmployeeBook {
+
+    public EmployeeBook(int i) {
+        this.employeeBook = new Employee[i];
+    }
+
+    public EmployeeBook(Employee[] employeeBook) { // вариант конструктора при наличии готового массива
+        this.employeeBook = employeeBook; // (только для внутреннего пользования, поэтому private)
+    }
+
+    private final Employee[] employeeBook;
+
+    // добавление нового сотрудника в массив
+    public boolean addEmployee(Employee newEmployee) {
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (employeeBook[i] == null) {
+                employeeBook[i] = newEmployee;
+                System.out.println("Сотрудник " + employeeBook[i].getName() + " принят на работу");
+                return true;
+            }
+        }
+        System.out.println("Найм нового сотрудника невозможен, нет мест в штатном расписании");
+        return false;
+    }
+
+    // поиск сотрудника по id
+    public Employee findEmployee(int id) {
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                if (employee.getId() == id) {
+                    return employee;
+                }
+            }
+        }
+        System.out.println("Сотрудник не найден");
+        return null;
+    }
+
+    // увольнение сотрудника
+    public boolean removeEmployee(int id) {
+        Employee employeeToRemove = findEmployee(id);
+        for (int i = 0; i < employeeBook.length; i++) {
+            if (employeeBook[i].equals(findEmployee(id))) {
+                System.out.println("Cотрудник " + employeeBook[i].getName() + " уволен");
+                employeeBook[i] = null;
+                return true;
+            }
+        }
+        System.out.println("Сотрудник не найден, увольнение невозможно");
+        return false;
+    }
+
+    // вывод всей базы сотрудников
+    public void printEmployees() {
+        System.out.println("Полная информация о сотрудниках компании:");
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    // расчет фонда оплаты труда
+    public double calculateSalaryFund() {
+        double sum = 0;
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                sum += employee.getSalary();
+            }
+        }
+        return sum;
+    }
+
+    // поиск минимальной зарплаты
+    public Employee findMinSalary() {
+        double minSalary = employeeBook[0].getSalary();
+        Employee minSalaryEmployee = employeeBook[0];
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                if (employee.getSalary() < minSalary) {
+                    minSalaryEmployee = employee;
+                    minSalary = employee.getSalary();
+                }
+            }
+        }
+        return minSalaryEmployee;
+    }
+
+    // поиск максимальной зарплаты
+    public Employee findMaxSalary() {
+        double maxSalary = employeeBook[0].getSalary();
+        Employee maxSalaryEmployee = employeeBook[0];
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                if (employee.getSalary() > maxSalary) {
+                    maxSalaryEmployee = employee;
+                    maxSalary = employee.getSalary();
+                }
+            }
+        }
+        return maxSalaryEmployee;
+    }
+
+    // поиск средней зарплаты
+    public double calculateAverageSalary() {
+        double averageSalary = 0;
+        int numberOfEmployees = 0; // сначала посчитаем, сколько у нас всего сотрудников
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                numberOfEmployees++;
+            }
+        }
+        System.out.println("Всего сотрудников: " + numberOfEmployees);
+        if (numberOfEmployees != 0) {
+            averageSalary = Math.floor(calculateSalaryFund() / numberOfEmployees);
+        }
+        return averageSalary;
+    }
+
+    // вывод списка сотрудников
+    public void printEmployeesNames() {
+        System.out.println("Список сотрудников:");
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
+
+    // индексирование зарплаты всем сотрудникам
+    public void indexAllSalaries(double percent) {
+        for (Employee employee : employeeBook) {
+            if (employee != null) {
+                double salary = Math.floor(employee.getSalary() * 100 * (1 + percent / 100) / 100);
+                employee.setSalary(salary);
+            }
+        }
+        System.out.println("Зарплата проиндексирована всем сотрудникам компании на " + percent + " процентов");
+    }
+
+    // вспомогательный метод для создания массива сотрудников в отделе
+    public Employee[] deptEmployeesArray(int department) {
+        Employee[] deptEmployees = new Employee[employeeBook.length];
+        // создадим пустой массив для сотрудников отдела, их не больше, чем всего сотрудников в компании
+        int numberOfDeptEmployees = 0; // пока не знаем, сколько сотрудников в отделе
+        for (Employee employee : employeeBook) {
+            if (employee != null && employee.getDepartment() == department) {
+                deptEmployees[numberOfDeptEmployees] = employee;
+                numberOfDeptEmployees++;
+            }
+        }
+        return deptEmployees;
+        // в рамках курсовой работы не будем усложнять метод и оставим массив сотрудников отдела
+        // изначально заданной длины и не будем переделывать с отбрасыванием null значений в конце
+    }
+
+    // поиск минимальной зарплаты в отделе
+    public Employee findMinDeptSalary(int department) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        EmployeeBook deptEmployeeBook = new EmployeeBook(deptEmployees);
+        return deptEmployeeBook.findMinSalary();
+    }
+
+    // поиск максимальной зарплаты в отделе
+    public Employee findMaxDeptSalary(int department) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        EmployeeBook deptEmployeeBook = new EmployeeBook(deptEmployees);
+        return deptEmployeeBook.findMaxSalary();
+    }
+
+    // расчет фонда оплаты труда в отделе
+    public double calculateDeptSalaryFund(int department) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        EmployeeBook deptEmployeeBook = new EmployeeBook(deptEmployees);
+        return deptEmployeeBook.calculateSalaryFund();
+    }
+
+    // поиск средней ЗП в отделе
+    public double calculateAverageDeptSalary(int department) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        EmployeeBook deptEmployeeBook = new EmployeeBook(deptEmployees);
+        return deptEmployeeBook.calculateAverageSalary();
+    }
+
+    // индексирование ЗП в отделе
+    public void indexDeptSalaries(int department, double percent) { // не придумал, как красиво обернуть, поэтому напрямую меняем
+        for (Employee employee : employeeBook) {
+            if (employee != null && employee.getDepartment() == department) {
+                double salary = Math.floor(employee.getSalary() * 100 * (1 + percent / 100) / 100);
+                employee.setSalary(salary);
+            }
+        }
+    }
+
+    // вывод списка сотрудников отдела
+    public void printDeptEmployeesNames(int department) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        System.out.println("Отдел " + department + ".");
+        EmployeeBook deptEmployeeBook = new EmployeeBook(deptEmployees);
+        deptEmployeeBook.printEmployeesNames();
+    }
+
+    // поиск сотрудников в отделе с меньшей ЗП
+    public void findLessThanDeptSalary(int department, double salary) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        System.out.println("В отделе " + department + " З/П меньше, чем " + salary + " у следующих сотрудников:");
+        for (Employee employee : deptEmployees) {
+            if (employee != null) {
+                if (employee.getSalary() < salary) {
+                    System.out.println("Сотрудник: " + employee.getName() + ", табельный номер: " + employee.getId() + ", З/П: " + employee.getSalary());
+                }
+            }
+        }
+    }
+
+    // поиск сотрудников в отделе с большей ЗП
+    public void findBiggerThanDeptSalary(int department, double salary) {
+        Employee[] deptEmployees = deptEmployeesArray(department);
+        System.out.println("В отделе " + department + " З/П больше, чем " + salary + " у следующих сотрудников:");
+        for (Employee employee : deptEmployees) {
+            if (employee != null) {
+                if (employee.getSalary() > salary) {
+                    System.out.println("Сотрудник: " + employee.getName() + ", табельный номер: " + employee.getId() + ", З/П: " + employee.getSalary());
+                }
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,42 @@
 public class Main {
     public static void main(String[] args) {
 
-        Employee[] employeeDatabase = new Employee[10];
+        EmployeeBook employeeBook = new EmployeeBook(10);
         // далее создадим массив сотрудников
         Employee ivanov1 = new Employee("Иванов Иван Иванович", 1, 100000); // создаем нового сотрудника
-        employeeDatabase[ivanov1.getId()] = ivanov1; // записываем его в массив сотрудников
+        employeeBook.addEmployee(ivanov1); // записываем его в массив сотрудников
         Employee petrov1 = new Employee("Петров Петр Петрович", 2, 120000);
-        employeeDatabase[petrov1.getId()] = petrov1;
+        employeeBook.addEmployee(petrov1);
         Employee ivanov2 = new Employee("Иванов Иван Иванович", 1, 100000); // создадим однофамильца Иванова Первого
-        employeeDatabase[ivanov2.getId()] = ivanov2;
+        employeeBook.addEmployee(ivanov2);
         Employee sidorov1 = new Employee("Сидоров Сидор Сидорович", 3, 123456);
-        employeeDatabase[sidorov1.getId()] = sidorov1;
+        employeeBook.addEmployee(sidorov1);
         Employee test = new Employee("test", 1, 999999); // создадим тестовую запись
-        employeeDatabase[test.getId()] = test;
-        employeeDatabase[test.getId()] = null; // чтобы тут же его уволить и создать дырку в перечне id
+        employeeBook.addEmployee(test);
         Employee nazarova1 = new Employee("Назарова Наталья Сергеевна", 2, 321654);
-        employeeDatabase[nazarova1.getId()] = nazarova1;
+        employeeBook.addEmployee(nazarova1);
         Employee stepanov1 = new Employee("Степанов Евгений Васильевич", 2, 999999);
-        employeeDatabase[stepanov1.getId()] = stepanov1;
+        employeeBook.addEmployee(stepanov1);
 
-//        Employee.printEmployees(employeeDatabase); // тестируем вывод всего массива сотрудников
-//        System.out.println(ivanov1.equals(ivanov2)); // сравним двух Ивановых
-//        System.out.println("Затраты на ЗП в месяц: " + Employee.calculateSalaryFund(employeeDatabase)); // тестируем подсчет фонда оплаты труда
-//        System.out.println("Сотрудник c минимальной ЗП: " + Employee.findMinSalary(employeeDatabase));
-//        System.out.println("Сотрудник c максимальной ЗП: " + Employee.findMaxSalary(employeeDatabase));
-//        System.out.println("Средняя ЗП: " + Employee.calculateAverageSalary(employeeDatabase));
-//        Employee.printEmployeesNames(employeeDatabase);
-//        Employee.indexAllSalaries(employeeDatabase, 10);
-//        Employee.printEmployees(employeeDatabase); // проверим, что ЗП проиндексирована
-//        System.out.println(Employee.findMinDeptSalary(employeeDatabase, 2));
-//        Employee.printEmployees(Employee.deptEmployeesArray(employeeDatabase, 2)); // тестируем вывод на печать сотрудников отдела используя общий метод печати
-//        System.out.println(Employee.findMinDeptSalary(employeeDatabase, 2)); // тестируем поиск минимальной ЗП в отделе
-//        Employee.printDeptEmployeesNames(employeeDatabase, 2); // тестируем вывод на печать сотрудников отдела
-//        Employee.indexDeptSalaries(employeeDatabase, 2, 10); // тестируем индексирование ЗП всему отделу
-//        Employee.printDeptEmployeesNames(employeeDatabase, 2); // проверяем, что ЗП проиндексирована
-//        System.out.println("Средняя ЗП: " + Employee.calculateAverageDeptSalary(employeeDatabase,2)); // тестируем подсчет средней ЗП в отделе
-//        Employee.findLessThanDeptSalary(employeeDatabase, 2, 400000); // тестируем уровень зарплат
-//        Employee.findBiggerThanDeptSalary(employeeDatabase, 2, 200000);
+        // тестируем методы
+        employeeBook.printEmployees(); // тестируем вывод всего массива сотрудников
+        employeeBook.removeEmployee(4); // уволим test, создав дырку в массиве
+        employeeBook.printEmployees();
+        System.out.println("Затраты на ЗП в месяц: " + employeeBook.calculateSalaryFund()); // тестируем подсчет фонда оплаты труда
+        System.out.println("Сотрудник c минимальной ЗП: " + employeeBook.findMinSalary());
+        System.out.println("Сотрудник c максимальной ЗП: " + employeeBook.findMaxSalary());
+        System.out.println("Средняя ЗП: " + employeeBook.calculateAverageSalary());
+        employeeBook.printEmployeesNames(); // тестируем вывод имен сотрудников
+        employeeBook.indexAllSalaries(10); // индексируем ЗП
+        employeeBook.printEmployees(); // проверим, что ЗП проиндексирована
+        System.out.println("В отделе 2 минимальную ЗП имеет " + employeeBook.findMinDeptSalary(2)); // тестируем поиск минимальной ЗП в отделе
+        System.out.println("В отделе 2 максимальную ЗП имеет " + employeeBook.findMaxDeptSalary(2)); // тестируем поиск максимальной ЗП в отделе
+        employeeBook.printDeptEmployeesNames(2); // тестируем вывод на печать сотрудников отдела
+        employeeBook.indexDeptSalaries(2, 10); // тестируем индексирование ЗП всему отделу
+        employeeBook.printDeptEmployeesNames(2); // проверяем, что ЗП проиндексирована
+        System.out.println("Средняя ЗП: " + employeeBook.calculateAverageDeptSalary(2)); // тестируем подсчет средней ЗП в отделе
+        employeeBook.findLessThanDeptSalary(2, 400000); // тестируем уровень зарплат
+        employeeBook.findBiggerThanDeptSalary(2, 200000);
 
     }
 }
